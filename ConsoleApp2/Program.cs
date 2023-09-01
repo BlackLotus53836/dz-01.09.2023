2df730172676fb18538d4eb9ed3bdc547545c976
Mark <Loop>
2023-09-01 18:31:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = new int[8];
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(100); // Генерация случайного числа от 0 до 99
+        }
+
+        Console.Write("Случайные числа: [");
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i]);
+
+            if (i < numbers.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+
+        Console.WriteLine("]");
+    }
+}
